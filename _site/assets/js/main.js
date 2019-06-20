5391d5e00a1edede98f18742bbac8ff6e7ed8958
DerrickGXD <goh831@gmail.com>
2019-06-20 18:21:07 +0100

    added components and our team page

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -4,7 +4,7 @@
  * Author: colorlib
  * Copyright (C) 2018 colorlib
  */
- /* ------------------------------------- */
+/* ------------------------------------- */
 /*  TABLE OF CONTENTS
  /* ------------------------------------- */
 /*   PRE LOADING                          */
@@ -15,28 +15,26 @@
 
 
 
-    /* ==============================================
+/* ==============================================
 /*  PRE LOADING
   =============================================== */
 'use strict';
-$(window).load(function() {
+$(window).load(function () {
     $('.loader').delay(500).fadeOut('slow');
 });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     'use strict';
     /* ==============================================
      /*   wow
       =============================================== */
-    var wow = new WOW(
-        {
-            animateClass: 'animated',
-            offset: 10,
-            mobile: true
-        }
-    );
+    var wow = new WOW({
+        animateClass: 'animated',
+        offset: 10,
+        mobile: true
+    });
     wow.init();
     /* ==============================================
         STICKY HEADER
@@ -59,7 +57,7 @@ $(document).ready(function() {
     });
 
     $(".countdown")
-        .countdown("2018/03/01", function(event) {
+        .countdown("2019/08/24 01:30:00", function (event) {
             $(this).html(
                 event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
@@ -69,63 +67,64 @@ $(document).ready(function() {
      SLIDER
      =============================================== */
     $(".cover_slider").owlCarousel({
-        loop:true,
-        autoplay:true,
-        smartSpeed:1000,
-        autoplayHoverPause:false,
-        dots:true,
-        nav:false,
-        items:1,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        autoplayHoverPause: false,
+        dots: true,
+        nav: false,
+        items: 1,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         dotsContainer: '.cover_dots'
     });
 
     $(".brand_carousel").owlCarousel({
-        loop:true,
-        autoplay:true,
-        smartSpeed:450,
-        autoplayHoverPause:false,
-        dots:false,
-        nav:false,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:2
+        loop: true,
+        autoplay: true,
+        smartSpeed: 450,
+        autoplayHoverPause: false,
+        dots: false,
+        nav: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
 
             },
-            1000:{
-                items:5
+            1000: {
+                items: 5
 
             }
         },
-        items:5
+        items: 5
     });
     /* ------------------------------------- */
     /* Animated progress bars
      /* ------------------------------------- */
 
-    var waypoints = $('.progress_container').waypoint(function() {
+    var waypoints = $('.progress_container').waypoint(function () {
         $('.progress .progress-bar').progressbar({
             transition_delay: 1000
         });
-    },{
+    }, {
         offset: '50%'
     });
 
 
-        /* --------------------------------------------------------
+    /* --------------------------------------------------------
     MAPS
     ----------------------------------------------------------- */
     var map = $('#map');
-    if(map.length > 0) {
+    if (map.length > 0) {
         google.maps.event.addDomListener(window, 'load', init);
         var lattuide = map.attr('data-lat');
         var longtuided = map.attr('data-lon');
     }
+
     function init() {
         // Basic options for a simple Google Map
         // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
@@ -141,7 +140,91 @@ $(document).ready(function() {
 
             // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
-            styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
+            styles: [{
+                "featureType": "water",
+                "stylers": [{
+                    "saturation": 43
+                }, {
+                    "lightness": -11
+                }, {
+                    "hue": "#0088ff"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "hue": "#ff0000"
+                }, {
+                    "saturation": -100
+                }, {
+                    "lightness": 99
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#808080"
+                }, {
+                    "lightness": 54
+                }]
+            }, {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ece2d9"
+                }]
+            }, {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ccdca1"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#767676"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            }, {
+                "featureType": "poi",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "landscape.natural",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "visibility": "on"
+                }, {
+                    "color": "#b8cb93"
+                }]
+            }, {
+                "featureType": "poi.park",
+                "stylers": [{
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "poi.sports_complex",
+                "stylers": [{
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "poi.medical",
+                "stylers": [{
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "poi.business",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }]
         };
 
         // Get the HTML DOM element that will contain your map
